6fa23f9b8b707c6768af102f9069536402f4243f
jasarqui <jzarquilita@up.edu.ph>
2018-11-19 21:20:44 +0800

    Add Create Lobby

--- TCPChat.java
+++ TCPChat.java
@@ -1,20 +1,41 @@
 import proto.TcpPacketProtos.*;
 import java.io.*;
 import java.net.*;
+import java.util.Arrays;
 
 public class TCPChat {
   public static void main(String[] args) {
     try {
-      // connect socket to 202.92.144.45/80
-      Socket server = new Socket("202.92.144.45", 80);
-      System.out.println("Just connected to " + server.getRemoteSocketAddress());    
+      // variables
+      String lobbyId;
       int choice = 0;
 
-      // while (choice != 3) {
-      //   print_menu();
-      // }
-      TcpPacket packet = TcpPacket.newBuilder();
+      // connect socket to 202.92.144.45/80
+      Socket server = new Socket("202.92.144.45", 80);
+      DataOutputStream out = new DataOutputStream(server.getOutputStream());
+      InputStream in = server.getInputStream();
+      System.out.println("Just connected to " + server.getRemoteSocketAddress());    
 
+      // create packet
+      TcpPacket.Builder packet = TcpPacket.newBuilder();
+      packet.setType(TcpPacket.PacketType.CONNECT);
+
+      // create lobby packet
+      TcpPacket.CreateLobbyPacket.Builder lobby = TcpPacket.CreateLobbyPacket.newBuilder();
+      lobby.setType(TcpPacket.PacketType.CREATE_LOBBY);
+      lobby.setMaxPlayers(4);
+
+      // send lobby packet bytes
+      out.write(lobby.build().toByteArray())
+      
+      // get reply
+      byte[] lobbyData = new byte[1024];
+      int count = in.read(lobbyData);
+      
+      // parse reply
+      lobbyData = Arrays.copyOf(lobbyData, count);
+      lobbyId = TcpPacket.CreateLobbyPacket.parseFrom(lobbyData).getLobbyId();
+      System.out.println(lobbyId);
 
     } catch(IOException e) { // error cannot connect to server
       e.printStackTrace();
